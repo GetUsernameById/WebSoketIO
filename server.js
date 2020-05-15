916f7533b00ba09048afe74b0386f2923a78998d
leontevdima123 <leontevdima123@gmail.com>
2020-05-15 12:57:57 +0800

    Redis + clusters

--- server.js
+++ server.js
@@ -1,122 +1,147 @@
 const express = require("express");
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+// const app = express();
+const net = require('net');
+const sio = require('socket.io');
+const redis = require('socket.io-redis');
+// const http = require('http');
+const farmhash = require('farmhash');
 const proc = require('process');
-
 
 let cluster = require('cluster');
 let port = 3000;
 
 
+
+
+
 if(cluster.isMaster){
-    
+
     const cpuCount = require('os').cpus().length;
-    const map = new Map();
+    let workers = [];
 
 
-    console.log(`cpuCount ${cpuCount}`);
+    //console.log(`cpuCount ${cpuCount}`);
     
     
     
+    //cpuCount - 1
+    for(let i = 0; i < 3; i++){
+        workers[i] = cluster.fork();
 
-    for(let i = 0; i < cpuCount; i++){
-        cluster.schedulingPolicy = cluster.SCHED_NONE;
-        cluster.fork();
+        
+        workers[i].on('fork', (worker) => {
+            // console.log(`Worker #${worker.id} is online!!`);
+        
+        })
+        workers[i].on('listening', (worker, adress) => {
+            // console.log(`Worker #${worker.id} is connected now to ${JSON.stringify(adress)}!!`);
+            
+        })
+        workers[i].on('dissconect', (worker) => {
+            console.log(`Worker #${worker.id} has disconnected!!`);
+            
+        })
+        workers[i].on('exit', (worker) => {
+            console.log(`Worker #${worker.id} is dead!!`);
+            cluster.fork();
+        })
     }
 
+    /**
+     *  Вспомогательная функция для получения рабочего индекса на основе IP-адреса.
+     *  IP-адрес преобразуется в число, удалив нечисловые символы, а затем сжимая его до количества имеющихся у нас процессов.
+     * 
+     * @param {*} ip IP-адрес
+     * @param {*} len Количество воркеров
+     */
+    let worker_index = function(ip, len) {
+		return farmhash.fingerprint32(ip) % len; // Farmhash is the fastest and works with IPv6, too
+    };
     
-
-    cluster.on('fork', (worker) => {
-        // console.log(`Worker #${worker.id} is online!!`);
-       
-    })
-    cluster.on('listening', (worker, adress) => {
-        // console.log(`Worker #${worker.id} is connected now to ${JSON.stringify(adress)}!!`);
-        
-    })
-    cluster.on('dissconect', (worker) => {
-        console.log(`Worker #${worker.id} has disconnected!!`);
-        
-    })
-    cluster.on('exit', (worker) => {
-        console.log(`Worker #${worker.id} is dead!!`);
-        cluster.fork();
-        
-    })
-
-    let numReq = 0;
-
+    /**
+     * Создайте внешний сервер, слушающий наш порт.
+     * Мы получили соединение и должны передать его соответствующему работнику. Получите рабочий для исходного IP-адреса этого соединения и передайте ему соединение.
+     */
+    console.log('IMA HERE');
     
-    for (const id in cluster.workers) {
-        cluster.workers[id].on('message', messageHandler);
-    }
-
-    function messageHandler(data){
-        console.log(data);
-        
-        map.set(data.payload, 123);
-    }
+    let server = net.createServer({ pauseOnConnect: true }, function(connection) {
+        let worker = workers[worker_index(connection.remoteAddress, 3)];
+        console.log(`IP ${connection.remoteAddress} `, worker_index(connection.remoteAddress, 3)); 
+		worker.send('sticky-session:connection', connection);
+	}).listen(port);
     
 }else{
+    const app = new express();
 
-    
-   
-    // console.log(process.pid);
-    proc.emit('message', 'Hello from worker');
     app.use(express.static('public'));
+
     app.get('/', (req, res) => {
+        
         res.sendFile(__dirname + '/public/index.html');
+        
     });
     
     app.get('/admin', (req, res) => {
+        console.log('GET CLUSTER ADMIN ', cluster.worker.id);
         res.sendFile(__dirname + '/public/admin.html');
     });
     
     app.get('/cashbox', (req, res) => {
+        console.log('GET CLUSTER CASHBOX ', cluster.worker.id);
         res.sendFile(__dirname + '/public/cashbox.html');
     });
-      
-    http.listen(port + cluster.worker.id, () => {
-        // console.log(`listening on *${port}`);
-    });
+
+    const server = app.listen(0, 'localhost');
+    const io = sio(server);
     
+    io.adapter(redis({ host: 'localhost', port: 6379 }));
+    // console.log(process.pid);
+    // proc.emit('message', 'Hello from worker');
+
+    process.on('message', function(message, connection) {
+		if (message !== 'sticky-session:connection') {
+			return;
+		}
+
+		// Эмулируем событие соединения на сервере, генерируя событие с подключением, которое нам прислал мастер
+		server.emit('connection', connection);
+
+		connection.resume();
+	});
+      
+
     let cashboxesCount = 0;
     
     io.of('admin').use((socket, next) =>{
         if (socket.handshake.query && socket.handshake.query.data){
             socket.authData = JSON.parse(socket.handshake.query.data); 
+            // console.log(socket.authData);
             next();
         } else {
             next(new Error('Authentication error'));
         }   
     }).on('connection', (socket) =>{
-    
+        console.log('clusterAdm ' + cluster.worker.id);
+        
         socket.join(`${socket.authData.role}/${socket.authData.organisationID}/${socket.authData.storeID}`);
         socket.join(`${socket.authData.role}/${socket.authData.organisationID}`);
         
-        
-        // process.send({type: "connection",payload: socket.id, env:process.argv});
-
+    
+        // let connectedCashboxes = io.of('/cashbox').clients().connected;
        
-    
-        let connectedCashboxes = io.of('/cashbox').clients().connected;
+        let connectedCashboxes = io.of('/cashbox').adapter.nsp.connected; 
         let cashboxData = []
-        io.of('/cashbox').in(`cashbox/${socket.authData.organisationID}/${socket.authData.storeID}`).clients((error, clients) => {
-            if (error) throw error;
-            
-            clients.forEach(client => {
-                cashboxData.push(connectedCashboxes[client].authData);
-            });
-            socket.emit('getActiveCashbox', cashboxData);
-        });
-    
         
-        
-        // console.log(connectedCashboxes);
-        
-        // let cashboxData = []
-        
+        if(Object.keys(connectedCashboxes).length > 0){
+            io.of('cashbox').in(`cashbox/${socket.authData.organisationID}/${socket.authData.storeID}`).clients((error, clients) => {
+                if (error) throw error;
+                clients.forEach(client => {
+                    let authData = connectedCashboxes[client].authData;
+                    cashboxData.push({...authData, socketID: connectedCashboxes[client].id});
+                });
+                socket.emit('getActiveCashbox', cashboxData);
+            }); 
+        }
         
         // for (client in connectedCashboxes) {
         //     console.log(client);
@@ -160,11 +185,16 @@ if(cluster.isMaster){
         if (socket.handshake.query && socket.handshake.query.data){
             socket.authData = JSON.parse(socket.handshake.query.data); 
             next();
+        
         } else {
             next(new Error('Authentication error'));
         }   
     }).on('connection', (socket) =>{
-    
+        console.log('clusterCB ' + cluster.worker.id);
+        // process.send({type: "connection",payload: socket.id, cluster : cluster.worker.id});
+        
+        
+        
         socket.join(`${socket.authData.role}/${socket.authData.organisationID}/${socket.authData.storeID}`);
         socket.join(`${socket.authData.role}/${socket.authData.organisationID}`);
     
@@ -178,7 +208,7 @@ if(cluster.isMaster){
     
     
         socket.on('disconnect', (reason) => {
-            console.log('Cb disconect')
+            console.log('Cb disconect', reason)
             io.of('admin')
                 .to(`admin/${socket.authData.organisationID}/${socket.authData.storeID}`)
                 .emit('dissconectCb', {...socket.authData, socketID: socket.id});
